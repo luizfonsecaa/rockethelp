3fd694b9784f61c998defb2e10a98239ccea9de8
Luiz Otavio <luiz.otavio@take5.com.br>
2022-07-27 20:48:49 -0300

    finish course

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import { NativeBaseProvider, StatusBar } from "native-base";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { THEME } from './src/styles/theme'
+import { SignIn } from './src/screens/Signin'
+import { Home } from './src/screens/Home'
+import { Routes } from './src/routes'
+import { Loading } from "./src/components/Loading";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
+
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparente"
+        translucent
+      />
+      {fontsLoaded ? <Routes /> : <Loading />}
+    </NativeBaseProvider>
+  );
+}
+
+
+
